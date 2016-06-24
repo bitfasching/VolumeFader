ede93146547196def52791e4930a21cc59dcbe27
Nick Schwarzenberg <nick@bitfasching.de>
2016-06-24 18:11:06 +0200

    Major redesign.
    - changed arguments and options of the constructor
    - using requestAnimationFrame instead of a constant interval
    - changed behaviour of start(), stop() and updateVolume()
    - introduced natural fading (by exponentially expanding the fading volume)

--- volumefader.js
+++ volumefader.js
@@ -1,138 +1,189 @@
 /**
  * VolumeFader
- * Linear Media Volume Fading
+ * Proper Media Volume Fading
  *
- * License: AGPLv3
+ * Requires browser support for:
+ * - HTMLMediaElement
+ * - requestAnimationFrame()
+ * - ES6
+ *
+ * Does not depend on any third-party library.
+ *
+ * License: MIT
  *
  * Nick Schwarzenberg
- * v0.1.0, 06/2016
+ * v0.2.0, 06/2016
  */
 
 ;(function( root ){
 
     'use strict'
 
-    // utility: check if number is a valid volume or duration
-    let validZeroToOne = (number) => ! Number.isNaN( number ) && number >= 0 && number <= 1
-    let validPositive = (number) => ! Number.isNaN( number ) && number > 0
+
+    // internal utility: check if value is a valid volume level and throw if not
+    let validateVolumeLevel = ( value ) =>
+    {
+        // number between 0 and 1?
+        if ( ! Number.isNaN( value ) && value >= 0 && value <= 1 )
+        {
+            // yup, that's fine
+            return
+        }
+        else
+        {
+            // abort and throw an exception
+            throw new TypeError( "Number between 0 and 1 expected for volume!" )
+        }
+    }
+
+
+    // internal utility: exponential scaler with 30 dB dynamic range (smallest value above zero is 0.001)
+    // (the input 0…1 is interpreted as logarithmic volume and expanded to a linear scale 0…1)
+    let exponentialScaler = ( logarithmic ) =>
+    {
+        // special case: make zero return zero
+        if ( logarithmic == 0 )
+        {
+            // since the dynamic range is limited,
+            // allow a zero to produce a plain zero instead of a small faction
+            // (audio would not be recognized as silent otherwise)
+            return 0
+        }
+        else
+        {
+            // scale to -30 dB
+            logarithmic = ( logarithmic - 1 ) * 3
+
+            // compute power of 10
+            return Math.pow( 10, logarithmic )
+        }
+    }
+
 
     // main class
     class VolumeFader
     {
-        // initialize new fader
-        constructor( mediaElement, initialVolume, fadeDuration, updateInterval, log )
+        /**
+         * VolumeFader Constructor
+         *
+         * @param media {HTMLMediaElement} - audio or video element to be controlled
+         * @param options {Object} - an object with optional settings
+         * @throws {TypeError} if options.initialVolume or options.fadeDuration are invalid
+         *
+         * options:
+         * .logger: {Function} logging `function(stuff, …)` for execution information
+         * .volumeScaler: {Function} scaling `function(value)` used to map fade levels 0…1 to volume levels 0…1
+         * .initialVolume: {Number} media volume 0…1 to apply during setup
+         * .fadeDuration: {Number} time in milliseconds to complete a fade
+         */
+        constructor( media, options )
         {
-            // set log preference
-            this.log = log || false
-
-            // if passed media element is not of correct type…
-            if ( ! mediaElement instanceof HTMLMediaElement )
+            // passed media element of correct type?
+            if ( media instanceof HTMLMediaElement )
+            {
+                // save reference to media element
+                this.media = media
+            }
+            else
             {
                 // abort and throw an exception
                 throw new TypeError( "Media element expected!" )
             }
 
-            // save reference to media element
-            this._media = mediaElement
+            // make sure options is an object
+            options = options || {}
 
-            // valid initial volume given?
-            if ( validZeroToOne( initialVolume ) )
+            // log function passed?
+            if ( options.logger instanceof Function )
             {
-                // set initial volume
-                this._media.volume = initialVolume
-            }
+                // set log function
+                this.logger = options.logger
 
-            try
-            {
-                // try to set fade duration to given value
-                this.setFadeDuration( fadeDuration )
-            }
-            catch ( error )
-            {
-                // inform about fallback
-                this.log && console.log( "[VolumeFader] Using default fade duration." )
-
-                // set default fade duration (500 ms)
-                this.setFadeDuration( 500 )
-            }
-
-            // if timer is not specifically disabled…
-            if ( updateInterval !== 0 && updateInterval !== false )
-            {
-                try
-                {
-                    // try to set update interval to given value
-                    this.setUpdateInterval( updateInterval )
-                }
-                catch ( error )
-                {
-                    // inform about fallback
-                    this.log && console.log( "[VolumeFader] Using default update interval." )
-
-                    // set update interval to default (50 ms)
-                    this.setUpdateInterval( 50 )
-                }
-
-                // start timer
-                this.start()
-            }
-
-            // initialization done
-            this.log && console.log( "[VolumeFader] Initialized for", this._media )
-        }
-
-
-        // getter for whether the fader is running (returns true if interval timer is set)
-        get active() { return !! this._intervalTimer }
-
-        // (re)start timer (must be running for volume updates)
-        start( updateInterval )
-        {
-            // update interval given?
-            if ( updateInterval !== undefined )
-            {
-                // set given update interval
-                this.setUpdateInterval( updateInterval )
-            }
-
-            // stop old timer if already running
-            this.active && this.stop()
-
-            // create new interval timer
-            this._intervalTimer = setInterval( this.updateVolume.bind( this ), this._updateInterval )
-
-            // return instance for chaining
-            return this
-        }
-
-        // stop timer (disabling volume updates)
-        stop()
-        {
-            // clear current interval timer
-            clearInterval( this._intervalTimer )
-
-            // return instance for chaining
-            return this
-        }
-
-
-        // set timer update interval
-        setUpdateInterval( updateInterval )
-        {
-            // if interval is a valid number > 0…
-            if ( validPositive( updateInterval ) )
-            {
-                // save timer update interval
-                this._updateInterval = updateInterval
-
-                // restart timer (but only if already running)
-                this.active && this.start()
+                // log setting
+                this.logger && this.logger( "[VolumeFader] Using custom logger:", this.logger )
             }
             else
             {
-                // abort and throw an exception
-                throw new TypeError( "Positive number expected for update interval!" )
+                // set log function explicitly to false
+                this.logger = false
             }
+
+            // volume scaling function given?
+            if ( options.volumeScaler !== undefined )
+            {
+                // valid function?
+                if ( options.volumeScaler instanceof Function )
+                {
+                    // set volume scaler
+                    this.volumeScaler = options.volumeScaler
+
+                    // log setting
+                    this.logger && this.logger( "[VolumeFader] Using custom volume scaler:", this.volumeScaler )
+                }
+                else
+                {
+                    // abort and throw exception
+                    throw new TypeError( "Expected function for volume scaler!" )
+                }
+            }
+            // no custom scaler defined?
+            else
+            {
+                // use default scaler (exponential with limited dynamic range)
+                this.volumeScaler = exponentialScaler
+            }
+
+            // initial volume given?
+            if ( options.initialVolume !== undefined )
+            {
+                // validate volume level and throw if invalid
+                validateVolumeLevel( options.initialVolume )
+
+                // set initial volume
+                this.media.volume = this.volumeScaler( options.initialVolume )
+
+                // log setting
+                this.logger && this.logger( "[VolumeFader] Set initial volume to " + String(this.media.volume) + "." )
+            }
+
+            // fade duration given?
+            if ( options.fadeDuration !== undefined )
+            {
+                // try to set given fade duration (will log if successful and throw if not)
+                this.setFadeDuration( options.fadeDuration )
+            }
+            else
+            {
+                // set default fade duration (500 ms)
+                this.fadeDuration = 500
+            }
+
+            // indicate that fader is not active yet
+            this.active = false
+
+            // initialization done
+            this.logger && this.logger( "[VolumeFader] Initialized for", this.media )
+        }
+
+
+        // (re)start update cycle (must be running for volume updates)
+        start()
+        {
+            // set fader to be active
+            this.active = true
+
+            // start by running the update method
+            this.updateVolume()
+
+            // return instance for chaining
+            return this
+        }
+
+        // stop update cycle (interrupting any fade)
+        stop()
+        {
+            // set fader to be inactive
+            this.active = false
 
             // return instance for chaining
             return this
@@ -143,10 +194,13 @@
         setFadeDuration( fadeDuration )
         {
             // if duration is a valid number > 0…
-            if ( validPositive( fadeDuration ) )
+            if ( ! Number.isNaN( fadeDuration ) && fadeDuration > 0 )
             {
                 // set fade duration
-                this._fadeDuration = fadeDuration
+                this.fadeDuration = fadeDuration
+
+                // log setting
+                this.logger && this.logger( "[VolumeFader] Set fade duration to " + String(fadeDuration) + " ms." )
             }
             else
             {
@@ -159,37 +213,46 @@
         }
 
 
-        // update media volume (called by interval timer)
+        // update media volume (calls itself using a timer)
         updateVolume()
         {
-            // is there a fade to process?
-            if ( this._fade )
+            // fader active and fade available to process?
+            if ( this.active && this.fade )
             {
-                // compute time left for fading
-                let remainingTime = this._fade.timestamp - Date.now()
+                // get current time
+                let now = Date.now()
 
                 // time left for fading?
-                if ( remainingTime > this._updateInterval )
+                if ( now < this.fade.time.end )
                 {
-                    // compute difference to target volume
-                    let volumeDelta = this._fade.volume - this._media.volume
+                    // compute current fade progress
+                    let progress = ( now - this.fade.time.start ) / ( this.fade.time.end - this.fade.time.start )
 
-                    // compute volume increment according to timer interval
-                    let volumeIncrement = volumeDelta * this._updateInterval / remainingTime
+                    // compute current level
+                    let level = progress * ( this.fade.volume.end - this.fade.volume.start ) + this.fade.volume.start
 
-                    // adjust volume
-                    this._media.volume += volumeIncrement
+                    // scale fade level to native linear volume and apply to media element
+                    this.media.volume = this.volumeScaler( level )
+
+                    // schedule next update
+                    root.requestAnimationFrame( this.updateVolume.bind( this ) )
                 }
                 else
                 {
+                    // log end of fade
+                    this.logger && this.logger( "[VolumeFader] Fade to " + String(this.fade.volume.end) + " complete." )
+
                     // time is up, jump to target volume
-                    this._media.volume = this._fade.volume
+                    this.media.volume = this.volumeScaler( this.fade.volume.end )
+
+                    // set fader to be inactive
+                    this.active = false
 
                     // done, call back (if callable)
-                    this._fade.callback instanceof Function && this._fade.callback()
+                    this.fade.callback instanceof Function && this.fade.callback()
 
                     // clear fade
-                    this._fade = undefined
+                    this.fade = undefined
                 }
             }
 
@@ -201,20 +264,22 @@
         // define new fade
         fadeTo( targetVolume, callback )
         {
-            // if no valid volume given…
-            if ( ! validZeroToOne( targetVolume ) )
-            {
-                // abort and throw an exception
-                throw new TypeError( "Number between 0 and 1 expected for volume!" )
-            }
+            // validate volume and throw if invalid
+            validateVolumeLevel( targetVolume )
 
-            // define fade towards given volume to complete in the future with an optional callback
-            this._fade =
+            // define new fade towards a given volume with an optional callback
+            this.fade =
             {
-                volume: targetVolume,
-                timestamp: Date.now() + this._fadeDuration,
+                volume: { start: this.media.volume, end: targetVolume },
+                time: { start: Date.now(), end: Date.now() + this.fadeDuration },
                 callback: callback
             }
+
+            // log new fade
+            this.logger && this.logger( "[VolumeFader] New fade:", this.fade )
+
+            // start fading
+            this.start()
 
             // return instance for chaining
             return this
